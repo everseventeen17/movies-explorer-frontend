adbdcbac64d58039383ac5f6d212872d019a1507
daniil <daniil.eudokimou@gmail.com>
2023-06-25 10:59:08 +0300

    after review v0.1

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -7,9 +7,9 @@ function SearchForm({onFilterChange, isFilter}) {
   return (
     <section className="search">
 
-      <form className="search__form" name="search" noValidate>
+      <form className="search__form" name="search">
         <div className="search__inner">
-          <input className="search__input"  name="search" placeholder="Фильм"/>
+          <input className="search__input"  name="search" placeholder="Фильм" required type="text"/>
           <button className="search__button" type="submit" onClick={handleSubmit} ></button>
         </div>
         <FilterCheckbox onFilterChange={onFilterChange} isFilter={isFilter}/>
